d9d9108c31cecd808f8db050a304d812e0553eba
Jonathan Ma <jonathanjma03@gmail.com>
2020-04-21 23:40:38 -0700

    basics

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
